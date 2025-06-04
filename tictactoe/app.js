dbd4d4def12c3234338305094b6d919fcdfcbb78
Oliverpe <olivier.persson@gmail.com>
2025-06-04 13:30:51 +0200

    fix some files

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
 //Filen app.js är den enda ni skall och tillåts skriva kod i.
+'use strict';
+
+const express = require('express');
+const { JSDOM } = require('jsdom'); // Fixad rad
+const cookieParser = require('cookie-parser');
+const globalObject = require('./servermodules');
+const fs = require('fs');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Servern är igång');
+});
+
+server.listen(3000, () => {
+    console.log('Servern är igång på port 3000!');
+});
